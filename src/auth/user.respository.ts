6d0449c2d24285550988f477e897ec7ca316b769
suwon <ysw7939@gmail.com>
2024-02-09 12:29:55 +0900

    🚚rename: 24.02.09 양수원 auth.repo -> user.repo

---
+++ src/auth/user.respository.ts
@@ -0,0 +1,34 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { DataSource, Repository } from "typeorm";
+import { User } from "./user.entity";
+import { Injectable } from "@nestjs/common";
+import * as bcrypt from 'bcryptjs';
+import { AuthCreateDto } from "./dto/auth-create-dto";
+
+
+@Injectable()
+export class UserRepository extends Repository<User> {
+    // 사용자 관련 커스텀 메서드들을 추가할 수 있음
+    constructor(dataSource: DataSource) {
+        super(User, dataSource.createEntityManager());
+    }
+
+    async createUser(authCreateDto: AuthCreateDto): Promise<void>{
+        const { address, passwd, email, nickname  } = authCreateDto;
+
+        const salt = await bcrypt.genSalt();
+        const hashedPasswd = await bcrypt.hash(passwd, salt);
+
+        const user = this.create({address, passwd: hashedPasswd, salt: salt, email:email, nickname:nickname})
+        try {
+            await this.save(user)
+        }catch (error) {
+            if(error.code === '23505') {
+                throw new ConflictException('Existing username');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
+    }
+    
+}

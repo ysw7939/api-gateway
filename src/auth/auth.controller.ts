18987300d7635402dd433dd54b61292237b9072c
suwon <ysw7939@gmail.com>
2024-02-09 21:56:53 +0900

    ✨feat: 24.02.09 양수원 auth/signup 로그인 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/common';
+import { AuthCreateDto } from './dto/auth-create-dto';
+import { AuthService } from './auth.service';
+
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    @Post('/signup')
+    signUp(@Body(ValidationPipe) authCreateDto: AuthCreateDto): Promise<void> {
+        return this.authService.signUp(authCreateDto);
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { UserRepository } from './user.respository';
+import { AuthCreateDto } from './dto/auth-create-dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        private userRepository: UserRepository,
+    ) { }
+
+    async signUp(authCreateDto: AuthCreateDto): Promise<void> {
+        return this.userRepository.createUser(authCreateDto);
+    }
+}

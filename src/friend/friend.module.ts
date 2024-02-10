46b641fec86024ef3eda60a71ba7eff1a4b3f9df
suwon <ysw7939@gmail.com>
2024-02-10 20:01:40 +0900

    ✨feat 24.02.10 양수원 friend comply

--- src/friend/friend.module.ts
+++ src/friend/friend.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
 import { FriendController } from './friend.controller';
 import { FriendService } from './friend.service';
-import { FriendRepository } from './friend.repository';
+import { FriendRequestRepository } from './friend.request.repository';
 import { UserRepository } from 'src/auth/user.respository';
 import { FriendRequest } from './friend.request.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { FriendRepository } from './friend.repository';
 
 @Module({
     imports: [
     TypeOrmModule.forFeature([FriendRequest])
   ],
   controllers: [FriendController],
-  providers: [FriendService, FriendRepository, UserRepository]
+  providers: [FriendService, FriendRequestRepository, UserRepository, FriendRepository]
 })
 export class FriendModule {}
--- src/friend/friend.service.ts
+++ src/friend/friend.service.ts
@@ -1,14 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { FriendRepository } from './friend.repository';
+import { FriendRequestRepository } from './friend.request.repository';
 import { UserRepository } from 'src/auth/user.respository';
 import { FriendRequestDto } from './dto/friend.request.dto';
 import { FriendRequest } from './friend.request.entity';
+import { FriendRepository } from './friend.repository';
+import { FriendComplyDto } from './dto/friend.comply.dto';
+import { Friend } from './friend.entity';
 
 @Injectable()
 export class FriendService {
     constructor(
-        private friendRepository: FriendRepository,
-        private userRepository: UserRepository
+        private friendRequestRepository: FriendRequestRepository,
+        private userRepository: UserRepository,
+        private friendRepository: FriendRepository
     ) { }
 
     async friendRequest(friendRequestDto: FriendRequestDto): Promise<FriendRequest>{
@@ -18,6 +22,19 @@ export class FriendService {
         const user2 = await this.userRepository.findUser(toUser)
 
 
-        return this.friendRepository.createFriend(user1,user2);
+        return this.friendRequestRepository.createFriend(user1,user2);
     }
+
+    async friendCreate(friendComplyDto: FriendComplyDto): Promise<Friend[]> {
+        const { friendRequestId } = friendComplyDto;
+       
+        const friendRequest = await this.friendRequestRepository.findOne({
+            where: { friendId: friendRequestId }, relations: {
+                toUser: true,
+                fromUser:true
+        } });
+        await this.friendRequestRepository.update(friendRequestId, { isFriend: true });
+        return await this.friendRepository.createFriend(friendRequest)
+    }
+
 }

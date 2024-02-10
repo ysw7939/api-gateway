c55113d0159a7baad8df93f1a54c9e83287f582d
suwon <ysw7939@gmail.com>
2024-02-10 17:32:38 +0900

    ✨feat 24.02.10 양수원 friend request

--- src/friend/friend.controller.ts
+++ src/friend/friend.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, ValidationPipe,Post,Body, Param,Get } from '@nestjs/common';
+import { FriendService } from './friend.service';
+import { FriendRequestDto } from './dto/friend.request.dto';
+import { FriendRequest } from './friend.request.entity';
 
 @Controller('friend')
-export class FriendController {}
+export class FriendController {
+    constructor(private friendService: FriendService) { }
+    
+    @Post('/request')
+    create(@Body(ValidationPipe) friendRequestDto: FriendRequestDto): Promise<FriendRequest> {
+        return this.friendService.friendRequest(friendRequestDto);
+    }
+}

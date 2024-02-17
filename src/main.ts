d63e295077c084ebe4d512c0d2aa185d2860d3a2
suwon <ysw7939@gmail.com>
2024-02-17 10:32:41 +0900

    ✨feat: 24.02.17 양수원 cors 설정

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { GlobalExceptionFilter } from './error/global.exceptrionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  //app.useGlobalFilters(new GlobalExceptionFilter);
+  app.useGlobalFilters(new GlobalExceptionFilter);
+  app.enableCors({
+    credentials: true,
+    origin: true
+  })
   await app.listen(3000);
 }
 bootstrap();

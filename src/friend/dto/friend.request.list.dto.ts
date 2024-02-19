9ca56b0d8c5bf61ab182e54601644ea1ad2d7aae
suwon <ysw7939@gmail.com>
2024-02-19 12:46:09 +0900

    📝docs: 24.02.19 양수원 스웨거 응답 dto 설명

--- src/friend/dto/friend.request.list.dto.ts
+++ src/friend/dto/friend.request.list.dto.ts
@@ -1,9 +1,38 @@
+import { ApiProperty } from "@nestjs/swagger"
 
 export class FriendRequestListDto {
+    @ApiProperty({
+        example: 1,
+        description: '친구신청 고유 식별 번호'
+    })
     friend_request_id: number
-    from_user: Date
+
+    @ApiProperty({
+        example: 2,
+        description: '신청한 유저 고유 식별 번호'
+    })
+    from_user: number
+
+    @ApiProperty({
+        example: 1,
+        description: '신청받은 유저 고유 식별 번호'
+    })
     to_user: number
+
+    @ApiProperty({
+        example: 1,
+        description: '친구 고유 식별 번호'
+    })
     is_friend: boolean
+
+    @ApiProperty({
+        description: '친구신청한 날짜 및 시간'
+    })
     created_at: Date
+
+    @ApiProperty({
+        example: false,
+        description: '친구신청받은 사용자가 읽었는지 여부'
+    })
     is_read: boolean
 } 
